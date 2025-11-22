cc65a0b66510859ad21e4d6ca4a77e66246b8d61
favouritekid <hapham1388@gmail.com>
2025-11-22 08:58:23 +0700

    fix duplicate alembic upgrade version

--- Backend_FastAPI/alembic/versions/a1b2c3d4e5f7_add_is_heavy_to_major_program.py
+++ Backend_FastAPI/alembic/versions/a1b2c3d4e5f7_add_is_heavy_to_major_program.py
@@ -1,6 +1,6 @@
 """add_is_heavy_to_major_program
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: a1b2c3d4e5f7
 Revises: 800336b03c5a
 Create Date: 2025-11-22 10:00:00.000000
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'a1b2c3d4e5f6'
+revision: str = 'a1b2c3d4e5f7'
 down_revision: Union[str, Sequence[str], None] = '800336b03c5a'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
